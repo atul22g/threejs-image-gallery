0d032b392356e757c8a44c9d4e23c3ae5d574bbe
Atul <atul22vcoders@gmaill.com>
2024-11-17 11:48:51 +0530

    remove line

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@
 */
 export default {
   // Set the base directory for GitHub pages
-  base: process.env.NODE_ENV === 'production' ? '/threejs-image-gallery/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/threejs-image-gallery/' : '/',
   build: {
     outDir: './dist',
     sourcemap: true,
